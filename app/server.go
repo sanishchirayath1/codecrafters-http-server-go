078e854bccdfb46098318834bc862d82e82a2e6b
sanish chirayath <sanishchirayath@gmail.com>
2023-11-26 21:47:45 +0530

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -6,8 +6,14 @@ import (
 	"os"
 )
 
+var (
+	IP      = "0.0.0.0"
+	PORT    = "4221"
+	IP_PORT = IP + ":" + PORT
+)
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", IP_PORT)
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -22,27 +28,27 @@ func main() {
 
 	fmt.Println("Listening on 4221")
 
+	defer func(l net.Listener) {
+		err := l.Close()
+		if err != nil {
+			fmt.Println("Unable to close connection: ", err)
+		}
+	}(l)
+
+	fmt.Println("Listening on " + IP_PORT)
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-
-		handleConnection(conn)
-	}
-}
-
-func handleConnection(conn net.Conn) {
-	response := "HTTP/1.1 200 OK\r\n\r\n"
-	_, err := conn.Write([]byte(response))
-	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
-	}
-
-	err = conn.Close()
-	if err != nil {
-		fmt.Println("Unable to close connection ", err)
-
+		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error while sending response ", err)
+		}
+		err = conn.Close()
+		if err != nil {
+			fmt.Println("Unable to close connection ", err)
+		}
 	}
 }
